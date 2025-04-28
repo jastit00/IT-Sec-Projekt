7b5571844f0ad13c4c8cc5994f2cd6e8244edd06
marcel.nicklass@gmx.de <marcel.nicklass@gmx.de>
2025-04-28 22:20:03 +0200

    feat: all events mit critical events tabelle verbunden

--- frontend/src/app/components/critical-events/critical-events.component.ts
+++ frontend/src/app/components/critical-events/critical-events.component.ts
@@ -1,10 +1,15 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EventService } from '../../services/event-service';
 
 @Component({
   selector: 'app-critical-events',
   templateUrl: './critical-events.component.html',
   styleUrls: ['./critical-events.component.scss']
 })
-export class CriticalEventsComponent {
 
+export class CriticalEventsComponent {
+  events: { id: number; date: string; relativeTime: string; event: string; status: string; ips: string[]; }[];
+  constructor(private eventService: EventService) { 
+    this.events = this.eventService.events;
+  }
 }
