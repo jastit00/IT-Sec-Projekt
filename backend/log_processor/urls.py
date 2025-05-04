1627401e6e6a9a664ec892036d358815ad77925d
jastit00 <jastit00@hs-esslingen.de>
2025-05-04 20:30:20 +0200

    feat: update urls.py

--- backend/log_processor/urls.py
+++ backend/log_processor/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('logfiles/', views.LogFileUploadView.as_view(), name='upload-log-file'),
-    path('audit_logs/', views.processed_logins, name='processed-logins'),
-    path('config_changes/', views.processed_config_changes, name='processed-config-changes'),
-    path('incidents/', views.processed_incidents, name='processed-incidents'),
+    path('logfiles/processed-logins/', views.processed_logins, name='processed-logins'),
+    path('logfiles/config_changes/', views.processed_config_changes, name='processed-config-changes'),
+    path('logfiles/incidents/', views.processed_incidents, name='processed-incidents'),
 ]
+
