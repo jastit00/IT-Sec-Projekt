f907e0e38694f89e92cb32050651343f0c9a34d9
T4URU2 <118301529+T4URU2@users.noreply.github.com>
2025-05-23 22:23:23 +0200

    feat: implementing test to detect several concurrent logins with logouts

--- backend/incident_detector/tests.py
+++ backend/incident_detector/tests.py
@@ -82,3 +82,16 @@ class ConcurrentLoginsDetectionTest(TestCase):
         result=detect_concurrent_logins()
         self.assertEqual(result["concurrent_logins"],3)
         self.assertEqual(ConcurrentLoginIncident.objects.count(),3)
+
+    def test_multiple_attack_mixed_w_logout(self):
+        # create the specific entries
+        UserLogin.objects.create(timestamp="2025-03-27 11:49:54.508+01", username="admin", src_ip_address="109.108.107.10", terminal="cdi8", result="success")
+        UserLogin.objects.create(timestamp="2025-03-27 11:49:54.508+01", username="admin", src_ip_address="109.108.108.10", terminal="cdj8", result="success")
+        UserLogin.objects.create(timestamp="2025-03-27 11:49:54.508+01", username="admin", src_ip_address="110.120.30.254", terminal="cdh9", result="success")
+        UserLogin.objects.create(timestamp="2025-03-27 11:49:54.508+01", username="admin", src_ip_address="200.168.10.10", terminal="cdc8", result="success")
+        UserLogout.objects.create(timestamp="2025-03-27 11:49:54.508+01", username="admin", terminal="cdc8")
+        
+        # test
+        result=detect_concurrent_logins()
+        self.assertEqual(result["concurrent_logins"],2)
+        self.assertEqual(ConcurrentLoginIncident.objects.count(),2)
