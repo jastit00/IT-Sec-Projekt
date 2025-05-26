b66ab05397124512bc376424db36970affae6b65
jastit00 <jastit00@hs-esslingen.de>
2025-05-26 13:16:44 +0200

    fix: naming

--- frontend/src/app/components/chart-four/chart-four.component.ts
+++ frontend/src/app/components/chart-four/chart-four.component.ts
@@ -80,7 +80,7 @@ ngOnInit(): void {
         labels: Object.keys(typeCountMap),
         datasets: [{
           //label: 'Login-Versuche pro IP',
-          data: Object.values(ipCountMap),
+          data: Object.values(typeCountMap),
           backgroundColor: ['#F94144', '#F3722C', '#F8961E', '#F9844A', '#F9C74F', '#90BE6D', '#43AA8B', '#4D908E', '#577590', '#277DA1']
         }]
       };
