61a5f7d8c741711ac44ed4cc7c5fa9b26e5d7148
T4URU2 <118301529+T4URU2@users.noreply.github.com>
2025-05-28 21:11:31 +0200

    feat: implementing test several config changes but no login to get ip

--- backend/incident_detector/tests.py
+++ backend/incident_detector/tests.py
@@ -202,7 +202,16 @@ class ConfigChangeDetectionTest(TestCase):
         result=detect_critical_config_change()
         self.assertEqual(result["critical_config_change"],1)
         self.assertEqual(ConfigIncident.objects.first().src_ip_address,UserLogin.objects.first().src_ip_address)
-        
+
+    def test_multiple_config_change_from_single_user(self):
+        # create the specific entries from file many_config_changes_single_login.log
+        self.entry_creator.make_entries('many_config_changes_single_login.log')
+        # test
+        result=detect_critical_config_change()
+        self.assertEqual(result["critical_config_change"],5)
+        self.assertEqual(UsysConfig.objects.count(),7)
+        for incident in ConfigIncident.objects.all():
+            self.assertEqual(incident.src_ip_address,UserLogin.objects.first().src_ip_address)
     def test_single_critical_change_no_previous_login(self):
         # create the specific entries from file config_change_alone.log
         self.entry_creator.make_entries('config_change_alone.log')
