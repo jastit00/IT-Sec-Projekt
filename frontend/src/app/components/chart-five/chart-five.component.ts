1ee6d243ff8b8da3d5a9280f9973d92dad3cffe1
jastit00 <jastit00@hs-esslingen.de>
2025-06-03 13:14:20 +0200

    fix: colors

--- frontend/src/app/components/chart-five/chart-five.component.ts
+++ frontend/src/app/components/chart-five/chart-five.component.ts
@@ -27,7 +27,7 @@ export class ChartFiveComponent implements OnInit {
     datasets: [{
       label: 'dos by source ip',
       data: [],
-      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF']
+      backgroundColor: ['#F94144', '#F3722C', '#F8961E', '#F9844A', '#F9C74F', '#90BE6D', '#43AA8B', '#4D908E', '#577590', '#277DA1']
     }]
   };
 ngOnInit(): void {
@@ -88,7 +88,7 @@ ngOnInit(): void {
         datasets: [{
           label: 'number of packets',
           data: dataValues,
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF']
+          backgroundColor: ['#F94144', '#F3722C', '#F8961E', '#F9844A', '#F9C74F', '#90BE6D', '#43AA8B', '#4D908E', '#577590', '#277DA1']
           
         }]
       };
