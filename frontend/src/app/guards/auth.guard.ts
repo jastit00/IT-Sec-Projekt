df36c25bb4840afbc740767bb4ef25dfa41a1471
marcel.nicklass@gmx.de <marcel.nicklass@gmx.de>
2025-06-05 11:37:57 +0200

    fix: keycloak endless redirect

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,17 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { keycloak } from './../auth/keycloak.service';
+import { environment } from '../../environments/environment.development';
 
 export const authGuard: CanActivateFn = async (route, state) => {
-  // Check if we're already in a redirect (to break the loop)
-  if (window.location.href.includes('code=') && window.location.href.includes('session_state=')) {
-    return true;
-  }
 
   if (keycloak.authenticated) {
     return true;
   } else {
     await keycloak.login({
-      redirectUri: window.location.origin + state.url
+      redirectUri: environment.redirectUri
     });
     return false;
   }
