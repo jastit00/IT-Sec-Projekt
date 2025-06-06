3f2f4675f8a2ca8625a5db5b3b7ec86752a75cf7
Haju-dev <michaja.hummel@proton.me>
2025-06-06 17:54:07 +0200

    fix: environments

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     keycloakUrl: 'http://localhost:8080',
-    redirectUri: 'http://localhost:4200/dashboard'
+    redirectUri: 'http://localhost:4200/dashboard',
+    backendUrl: 'http://localhost:8000'
 };
