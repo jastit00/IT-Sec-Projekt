d554bf6c1e43f9b368799251a2fa2753d13667fb
T4URU2 <118301529+T4URU2@users.noreply.github.com>
2025-06-08 22:29:28 +0200

    fix: updating function concurrent logins to newest version (compare branch 36)

--- backend/incident_detector/services.py
+++ backend/incident_detector/services.py
@@ -496,24 +496,47 @@ def detect_ddos_attack(config):
 
 def detect_concurrent_logins():
     """
-    Detects and logs simultaneous logins without a corresponding logout.
-
+    Purpose:
+    Detect and log incidents for simutaneous logins.
+    
+    How:
+    Checks whether before next login of the same account a logout or a session termination happened.
+    Session terminations have in logs type USER_END but are stored in the db as USER_LOGOUT.
+    
     Returns:
-        dict: Number of simultaneous login incidents created.
+    dict {"concurrent_logins": <number of incidents created>, "incidents": <list with all the new created incidents>}
     """
-    new_incidents = []
-    potential_used_accounts=[]
-    successful_logins = UserLogin.objects.all().filter(result="success")
+    new_incidents=[]
+    potential_used_accounts={}
+    incidents_created = 0
+    successful_logins=UserLogin.objects.filter(result="success")
     for login in successful_logins:
-        if (UserLogout.objects.filter(terminal=login.terminal).count())==0:
-            if login.username in potential_used_accounts:
-                if not ConcurrentLoginIncident.objects.filter(src_ip_address=login.src_ip_address,username=login.username,incident_type="concurrentLogin").exists():
-                    incident = ConcurrentLoginIncident.objects.create(timestamp=login.timestamp,username=login.username,src_ip_address=login.src_ip_address,reason="user logged in again without previous logout")
-                    new_incidents.append(incident)
+        if login.username in potential_used_accounts:
+            if (
+                (UserLogout.objects.filter(terminal=potential_used_accounts[login.username]).count()==1) and 
+                (UserLogout.objects.filter(terminal=potential_used_accounts[login.username]).first().timestamp < login.timestamp)
+               ):
+                potential_used_accounts[login.username]=login.terminal
             else:
-                potential_used_accounts.append(login.username)
+                if not ConcurrentLoginIncident.objects.filter(
+                    src_ip_address=login.src_ip_address,
+                    username=login.username,
+                    incident_type="concurrentLogin"
+                    ).exists():
+                    incident = ConcurrentLoginIncident.objects.create(
+                        timestamp=login.timestamp,
+                        username=login.username,
+                        src_ip_address=login.src_ip_address,
+                        reason="user logged in again without previous logout"
+                    )
+                    incidents_created += 1
 
-    return {"concurrent_logins": len(new_incidents), "incidents": new_incidents}
+                    new_incidents.append(incident)
+                    potential_used_accounts[login.username]=login.terminal
+        else:
+            potential_used_accounts[login.username]=login.terminal
+
+    return {"concurrent_logins": incidents_created, "incidents": new_incidents}
 
 
 def format_timedelta(delta):
