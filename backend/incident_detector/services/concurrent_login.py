5582055a104413e58cd21ea1d89ee191f9637c8e
T4URU2 <118301529+T4URU2@users.noreply.github.com>
2025-06-11 13:23:44 +0200

    fix: upadting function to correct form and cleaning
    cleaning: formatting imports; adding descriptive comment

--- backend/incident_detector/services/concurrent_login.py
+++ backend/incident_detector/services/concurrent_login.py
@@ -1,29 +1,50 @@
-from incident_detector.models import (
-    ConcurrentLoginIncident,
-)
+from incident_detector.models import ConcurrentLoginIncident
 from log_processor.models import (
     UserLogin,
-    UserLogout,
+    UserLogout
 )
-
 
 def detect_concurrent_logins():
     """
-    Detects and logs simultaneous logins without a corresponding logout.
-
-    Returns:
-        dict: Number of simultaneous login incidents created.
-    """
-    new_incidents = []
-    potential_used_accounts=[]
-    successful_logins = UserLogin.objects.all().filter(result="success")
-    for login in successful_logins:
-        if (UserLogout.objects.filter(terminal=login.terminal).count())==0:
-            if login.username in potential_used_accounts:
-                if not ConcurrentLoginIncident.objects.filter(src_ip_address=login.src_ip_address,username=login.username,incident_type="concurrentLogin").exists():
-                    incident = ConcurrentLoginIncident.objects.create(timestamp=login.timestamp,username=login.username,src_ip_address=login.src_ip_address,reason="user logged in again without previous logout")
-                    new_incidents.append(incident)
-            else:
-                potential_used_accounts.append(login.username)
+    Purpose:
+    Detect and log incidents for simutaneous logins.
     
-    return {"concurrent_logins": len(new_incidents), "incidents": new_incidents}
+    How:
+    Checks whether before next login of the same account a logout or a session termination happened.
+    Session terminations have in logs type USER_END but are stored in the db as USER_LOGOUT.
+    
+    Returns:
+    dict {"concurrent_logins": <number of incidents created>, "incidents": <list with all the new created incidents>}
+    """
+    
+    new_incidents=[]
+    potential_used_accounts={}
+    incidents_created = 0
+    successful_logins=UserLogin.objects.filter(result="success")
+    for login in successful_logins:
+        if login.username in potential_used_accounts:
+            if (
+                (UserLogout.objects.filter(terminal=potential_used_accounts[login.username]).count()==1) and 
+                (UserLogout.objects.filter(terminal=potential_used_accounts[login.username]).first().timestamp < login.timestamp)
+               ):
+                potential_used_accounts[login.username]=login.terminal
+            else:
+                if not ConcurrentLoginIncident.objects.filter(
+                    src_ip_address=login.src_ip_address,
+                    username=login.username,
+                    incident_type="concurrentLogin"
+                    ).exists():
+                    incident = ConcurrentLoginIncident.objects.create(
+                        timestamp=login.timestamp,
+                        username=login.username,
+                        src_ip_address=login.src_ip_address,
+                        reason="user logged in again without previous logout"
+                    )
+                    incidents_created += 1
+
+                    new_incidents.append(incident)
+                    potential_used_accounts[login.username]=login.terminal
+        else:
+            potential_used_accounts[login.username]=login.terminal
+
+    return {"concurrent_logins": incidents_created, "incidents": new_incidents}
